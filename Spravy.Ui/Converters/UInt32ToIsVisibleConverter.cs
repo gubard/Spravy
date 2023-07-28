35cc63a6430b72c878ec061b2d58dc4ae3b42f15
Serhii Maksymov <maksimovsergey08@gmail.com>
2023-07-28 13:36:16 +0300

    add UInt32ToIsVisibleConverter

---
+++ Spravy.Ui/Converters/UInt32ToIsVisibleConverter.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Spravy.Ui.Converters;
+
+public class UInt32ToIsVisibleConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not uint item)
+        {
+            return false;
+        }
+
+        return item > 0;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
