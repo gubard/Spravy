798042bd0f05f79a94f2b8c674153fe328670894
Serhii Maksymov <maksimovsergey08@gmail.com>
2023-08-11 11:12:11 +0300

    add scroll buttons

--- Spravy.Ui/Views/ToDoSubItemsView.axaml.cs
+++ Spravy.Ui/Views/ToDoSubItemsView.axaml.cs
@@ -1,4 +1,8 @@
+using System;
 using System.Windows.Input;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
 using Avalonia.ReactiveUI;
 using ExtensionFramework.Core.Common.Extensions;
 using Spravy.Ui.Interfaces;
@@ -11,6 +15,14 @@ public partial class ToDoSubItemsView : ReactiveUserControl<ToDoSubItemsViewMode
     ICompleteSubToDoItemCommand,
     IChangeCurrentStatusToDoItemCommand
 {
+    public const string MainScrollViewerName = "MainScrollViewer";
+    public const string ScrollUpButtonName = "ScrollUpButton";
+    public const string ScrollButtonBottomName = "ScrollButtonBottom";
+
+    private ScrollViewer? mainScrollViewer;
+    private Button? scrollUpButton;
+    private Button? scrollButtonBottom;
+
     public ToDoSubItemsView()
     {
         InitializeComponent();
@@ -22,4 +34,33 @@ public partial class ToDoSubItemsView : ReactiveUserControl<ToDoSubItemsViewMode
     public ICommand AddSubToDoItemToCurrentCommand => ViewModel.ThrowIfNull().AddSubToDoItemToCurrentCommand;
     public ICommand RemoveSubToDoItemFromCurrentCommand => ViewModel.ThrowIfNull().RemoveSubToDoItemFromCurrentCommand;
     public ICommand ChangeToActiveDoItemCommand => ViewModel.ThrowIfNull().ChangeToActiveDoItemCommand;
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+        mainScrollViewer = this.FindControl<ScrollViewer>(MainScrollViewerName).ThrowIfNull();
+        SetupButton(ref scrollUpButton, ScrollUpButtonName, ScrollUp);
+        SetupButton(ref scrollButtonBottom, ScrollButtonBottomName, ScrollBottom);
+    }
+
+    private void SetupButton(ref Button? button, string name, EventHandler<RoutedEventArgs> handler)
+    {
+        if (button is not null)
+        {
+            button.Click -= handler;
+        }
+
+        button = this.FindControl<Button>(name).ThrowIfNull();
+        button.Click += handler;
+    }
+
+    private void ScrollUp(object? sender, RoutedEventArgs e)
+    {
+        mainScrollViewer?.ScrollToHome();
+    }
+
+    private void ScrollBottom(object? sender, RoutedEventArgs e)
+    {
+        mainScrollViewer?.ScrollToEnd();
+    }
 }
