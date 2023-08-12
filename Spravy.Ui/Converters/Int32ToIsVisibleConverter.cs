c7620d0155da60e0ef9b29946c101b78ca7c5407
Serhii Maksymov <maksimovsergey08@gmail.com>
2023-08-12 15:57:15 +0300

    fix adaptive ToDoSubItemsView

---
+++ Spravy.Ui/Converters/Int32ToIsVisibleConverter.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Spravy.Ui.Converters;
+
+public class Int32ToIsVisibleConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is int i)
+        {
+            if (i > 0)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
