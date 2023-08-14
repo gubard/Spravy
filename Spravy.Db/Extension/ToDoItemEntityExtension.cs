f8fc77e1f907de23189d5f7e0cffd00b2a47c60d
Serhii Maksymov <maksimovsergey08@gmail.com>
2023-08-14 08:06:26 +0300

    fix GetDays

--- Spravy.Db/Extension/ToDoItemEntityExtension.cs
+++ Spravy.Db/Extension/ToDoItemEntityExtension.cs
@@ -8,16 +8,31 @@ public static class ToDoItemEntityExtension
 {
     public static IEnumerable<byte> GetDaysOfMonth(this ToDoItemEntity item)
     {
+        if (item.DaysOfMonth.IsNullOrWhiteSpace())
+        {
+            return Enumerable.Empty<byte>();
+        }
+
         return item.DaysOfMonth.Split(";").Select(byte.Parse);
     }
 
     public static IEnumerable<DayOfWeek> GetDaysOfWeek(this ToDoItemEntity item)
     {
+        if (item.DaysOfWeek.IsNullOrWhiteSpace())
+        {
+            return Enumerable.Empty<DayOfWeek>();
+        }
+
         return item.DaysOfWeek.Split(";").Select(Enum.Parse<DayOfWeek>);
     }
 
     public static IEnumerable<DayOfYear> GetDaysOfYear(this ToDoItemEntity item)
     {
+        if (item.DaysOfYear.IsNullOrWhiteSpace())
+        {
+            return Enumerable.Empty<DayOfYear>();
+        }
+
         return item.DaysOfYear.Split(";")
             .Select(
                 x =>
@@ -28,17 +43,17 @@ public static class ToDoItemEntityExtension
                 }
             );
     }
-    
+
     public static void SetDaysOfYear(this ToDoItemEntity item, IEnumerable<DayOfYear> daysOfYear)
     {
         item.DaysOfYear = daysOfYear.Select(x => $"{x.Month}.{x.Day}").JoinString(";");
     }
-    
+
     public static void SetDaysOfMonth(this ToDoItemEntity item, IEnumerable<byte> daysOfMonth)
     {
         item.DaysOfMonth = daysOfMonth.JoinString(";");
     }
-    
+
     public static void SetDaysOfWeek(this ToDoItemEntity item, IEnumerable<DayOfWeek> daysOfWeek)
     {
         item.DaysOfWeek = daysOfWeek.JoinString(";");
