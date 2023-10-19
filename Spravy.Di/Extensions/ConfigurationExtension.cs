b70813533905bab5ccdf7e0068aebb6f5aed2291
Serhii Maksymov <maksimovsergey08@gmail.com>
2023-10-19 17:07:52 +0300

    edit config

--- Spravy.Di/Extensions/ConfigurationExtension.cs
+++ Spravy.Di/Extensions/ConfigurationExtension.cs
@@ -10,4 +10,9 @@ public static class ConfigurationExtension
     {
         return configuration.GetSection(T.Section).Get<T>().ThrowIfNull();
     }
+    
+    public static T GetConfigurationSection<T>(this IConfiguration configuration, string path) where T : IOptionsValue
+    {
+        return configuration.GetSection(path).Get<T>().ThrowIfNull();
+    }
 }
--- Spravy.Ui.Browser/Configurations/BrowserModule.cs
+++ Spravy.Ui.Browser/Configurations/BrowserModule.cs
@@ -1,6 +1,12 @@
 using Microsoft.Extensions.Configuration;
 using Ninject.Modules;
+using Spravy.Authentication.Domain.Client.Models;
+using Spravy.Di.Extensions;
+using Spravy.Domain.Extensions;
 using Spravy.Domain.Helpers;
+using Spravy.EventBus.Domain.Client.Models;
+using Spravy.Schedule.Domain.Client.Models;
+using Spravy.ToDo.Domain.Client.Models;
 
 namespace Spravy.Ui.Browser.Configurations;
 
@@ -10,11 +16,32 @@ public class BrowserModule : NinjectModule
 
     public override void Load()
     {
+        Bind<GrpcAuthenticationServiceOptions>()
+            .ToMethod(
+                context => context.Kernel.GetConfigurationSection<GrpcAuthenticationServiceOptions>("GrpcRouterService")
+            );
+
+        Bind<GrpcScheduleServiceOptions>()
+            .ToMethod(
+                context => context.Kernel.GetConfigurationSection<GrpcScheduleServiceOptions>("GrpcRouterService")
+            );
+
+        Bind<GrpcToDoServiceOptions>()
+            .ToMethod(
+                context => context.Kernel.GetConfigurationSection<GrpcToDoServiceOptions>("GrpcRouterService")
+            );
+
+        Bind<GrpcEventBusServiceOptions>()
+            .ToMethod(
+                context => context.Kernel.GetConfigurationSection<GrpcEventBusServiceOptions>("GrpcRouterService")
+            );
+
         Bind<IConfiguration>()
             .ToMethod(
                 _ =>
                 {
-                    using var stream = SpravyUiBrowserMark.GetResourceStream(FileNames.DefaultConfigFileName);
+                    using var stream = SpravyUiBrowserMark.GetResourceStream(FileNames.DefaultConfigFileName)
+                        .ThrowIfNull();
 
                     return new ConfigurationBuilder().AddJsonStream(stream).Build();
                 }
