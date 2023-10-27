59e3754cb08d1850a9175322e33c67173cfb0d63
Serhii Maksymov <maksimovsergey08@gmail.com>
2023-10-27 12:05:35 +0300

    remove Scroll buttons

--- Spravy.Ui/Views/ToDoSubItemsView.axaml.cs
+++ Spravy.Ui/Views/ToDoSubItemsView.axaml.cs
@@ -15,10 +15,6 @@ public partial class ToDoSubItemsView : MainReactiveUserControl<ToDoSubItemsView
     ICompleteSubToDoItemCommand,
     IChangePinnedToDoItemCommand
 {
-    public const string MainScrollViewerName = "MainScrollViewer";
-    public const string ScrollUpButtonName = "ScrollUpButton";
-    public const string ScrollButtonBottomName = "ScrollButtonBottom";
-
     private ScrollViewer? mainScrollViewer;
     private Button? scrollUpButton;
     private Button? scrollButtonBottom;
@@ -34,33 +30,4 @@ public partial class ToDoSubItemsView : MainReactiveUserControl<ToDoSubItemsView
     public ICommand AddSubToDoItemToPinnedCommand => ViewModel.ThrowIfNull().AddSubToDoItemToPinnedCommand;
     public ICommand RemoveSubToDoItemFromPinnedCommand => ViewModel.ThrowIfNull().RemoveSubToDoItemFromPinnedCommand;
     public ICommand ChangeToActiveDoItemCommand => ViewModel.ThrowIfNull().ChangeToActiveDoItemCommand;
-
-    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
-    {
-        base.OnApplyTemplate(e);
-        mainScrollViewer = this.FindControl<ScrollViewer>(MainScrollViewerName).ThrowIfNull();
-        SetupButton(ref scrollUpButton, ScrollUpButtonName, ScrollUp);
-        SetupButton(ref scrollButtonBottom, ScrollButtonBottomName, ScrollBottom);
-    }
-
-    private void SetupButton(ref Button? button, string name, EventHandler<RoutedEventArgs> handler)
-    {
-        if (button is not null)
-        {
-            button.Click -= handler;
-        }
-
-        button = this.FindControl<Button>(name).ThrowIfNull();
-        button.Click += handler;
-    }
-
-    private void ScrollUp(object? sender, RoutedEventArgs e)
-    {
-        mainScrollViewer?.ScrollToHome();
-    }
-
-    private void ScrollBottom(object? sender, RoutedEventArgs e)
-    {
-        mainScrollViewer?.ScrollToEnd();
-    }
 }
