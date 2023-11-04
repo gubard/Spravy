c876c41977a1d1c174f06a7acd69bc8380dcab9e
Serhii Maksymov <maksimovsergey08@gmail.com>
2023-11-04 15:51:57 +0200

    add logs

--- build/Extensions/SshClientExtension.cs
+++ build/Extensions/SshClientExtension.cs
@@ -8,6 +8,16 @@ public static class SshClientExtension
     public static void SafeRun(this SshClient client, string command)
     {
         using var runCommand = client.RunCommand(command);
-        Log.Logger.Information("Run SSH command {Command}", command);
+        Log.Logger.Information("Run SSH command:{Command}", command);
+
+        if (!string.IsNullOrWhiteSpace(runCommand.Error))
+        {
+            Log.Error("SSH Error: {Error}", runCommand.Error);
+        }
+
+        if (!string.IsNullOrWhiteSpace(runCommand.Result))
+        {
+            Log.Error("SSH result: {Error}", runCommand.Result);
+        }
     }
 }
