5a2eb4850ae5fdd776e89b6a7c398ac74f24e103
Serhii Maksymov <maksimovsergey08@gmail.com>
2023-11-05 19:09:49 +0200

    remove logs

--- Spravy.Di/Extensions/ConfigurationExtension.cs
+++ Spravy.Di/Extensions/ConfigurationExtension.cs
@@ -9,7 +9,6 @@ public static class ConfigurationExtension
     public static T GetConfigurationSection<T>(this IConfiguration configuration) where T : IOptionsValue
     {
         var section = configuration.GetSection(T.Section);
-        Console.WriteLine($"{T.Section} {section.Value} {section.GetChildren().Select(x => x.Value).JoinString(";")}");
 
         return section.Get<T>().ThrowIfNull();
     }
@@ -17,7 +16,6 @@ public static class ConfigurationExtension
     public static T GetConfigurationSection<T>(this IConfiguration configuration, string path) where T : IOptionsValue
     {
         var section = configuration.GetSection(path);
-        Console.WriteLine($"{T.Section} {section.Value} {section.GetChildren().Select(x => x.Value).JoinString(";")}");
 
         return section.Get<T>().ThrowIfNull();
     }
