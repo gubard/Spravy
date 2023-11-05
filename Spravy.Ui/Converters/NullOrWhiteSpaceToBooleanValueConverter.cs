fafeff3a44f2c8dd5cc15dda4518579b7944091b
Serhii Maksymov <maksimovsergey08@gmail.com>
2023-11-05 09:46:06 +0200

    refactor Ui

---
+++ Spravy.Ui/Converters/NullOrWhiteSpaceToBooleanValueConverter.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Spravy.Domain.Extensions;
+
+namespace Spravy.Ui.Converters;
+
+public class NullOrWhiteSpaceToBooleanValueConverter : IValueConverter
+{
+    public bool Reverse { get; set; }
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not string str)
+        {
+            return false;
+        }
+
+        if (Reverse)
+        {
+            return !str.IsNullOrWhiteSpace();
+        }
+
+        return str.IsNullOrWhiteSpace();
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
