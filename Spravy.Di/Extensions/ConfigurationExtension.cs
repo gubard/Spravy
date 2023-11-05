35c808c86eb0741dcbe28b6903fb5952f1fc199b
Serhii Maksymov <maksimovsergey08@gmail.com>
2023-11-05 17:33:15 +0200

    add logs

--- Spravy.Di/Extensions/ConfigurationExtension.cs
+++ Spravy.Di/Extensions/ConfigurationExtension.cs
@@ -8,13 +8,15 @@ public static class ConfigurationExtension
 {
     public static T GetConfigurationSection<T>(this IConfiguration configuration) where T : IOptionsValue
     {
+        Console.WriteLine($"{T.Section} {configuration.GetSection(T.Section).Value}");
+
         return configuration.GetSection(T.Section).Get<T>().ThrowIfNull();
     }
 
     public static T GetConfigurationSection<T>(this IConfiguration configuration, string path) where T : IOptionsValue
     {
         Console.WriteLine($"{path} {configuration.GetSection(path).Value}");
-        
+
         return configuration.GetSection(path).Get<T>().ThrowIfNull();
     }
 }
