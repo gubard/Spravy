bac7be86bf65ced4e8a9846da0be5ff57e6ace71
Serhii Maksymov <maksimovsergey08@gmail.com>
2023-11-23 20:42:52 +0200

    refactor

--- Spravy.Ui/Converters/ToDoItemIsCanToBooleanConverter.cs
+++ Spravy.Ui/Converters/ToDoItemIsCanToBooleanConverter.cs
@@ -7,7 +7,7 @@ namespace Spravy.Ui.Converters;
 
 public class ToDoItemIsCanToBooleanConverter : IValueConverter
 {
-    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (value is not ToDoItemIsCan isCan)
         {
@@ -21,8 +21,8 @@ public class ToDoItemIsCanToBooleanConverter : IValueConverter
         };
     }
 
-    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        throw new NotSupportedException();
     }
 }
