5dcff72d39e548daadc914e8d3d45be44a79b216
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-01-12 14:31:28 +0200

    Enhance TextBoxExtension to handle focus
    The TextBoxExtension has been updated to ensure the textbox is focused before clearing its text. This was achieved by adding a 'MustFocused' assertion and a loop for pressing and releasing the ArrowLeft key for each character in the textbox. This ensures programmatic selection of text in the textbox before deletion.

--- Spravy.Tests/Extensions/TextBoxExtension.cs
+++ Spravy.Tests/Extensions/TextBoxExtension.cs
@@ -10,6 +10,15 @@ public static class TextBoxExtension
     public static TTextBox ClearText<TTextBox>(this TTextBox textBox, Window window) where TTextBox : TextBox
     {
         textBox.Text.ThrowIfNullOrEmpty();
+        textBox.MustFocused();
+
+        for (var i = 0; i < textBox.Text.ThrowIfNull().Length; i++)
+        {
+            window.KeyPressQwerty(PhysicalKey.ArrowLeft, RawInputModifiers.None);
+            window.RunJobsAll();
+            window.KeyReleaseQwerty(PhysicalKey.ArrowLeft, RawInputModifiers.None);
+            window.RunJobsAll();
+        }
 
         while (textBox.Text.ThrowIfNull().Length > 0)
         {
