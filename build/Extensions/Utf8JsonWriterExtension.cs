0697ff229a5b7738606905f09f82854bd9fa95af
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-01-12 17:23:31 +0200

    Add logging to Utf8JsonWriterExtension
    The code now logs two pieces of information in the Utf8JsonWriterExtension: when the URL is added and when the certificate is added. This change improves the

--- build/Extensions/Utf8JsonWriterExtension.cs
+++ build/Extensions/Utf8JsonWriterExtension.cs
@@ -46,13 +46,16 @@ public static class Utf8JsonWriterExtension
                     writer.AddObject("HttpsInlineCertAndKeyFile", () =>
                         {
                             writer.AddStringValue("Url", $"https://0.0.0.0:{port}");
-
+                            Log.Information("Add Url: https://0.0.0.0:{Port}", port);
+                            
                             writer.AddObject("Certificate", () =>
                                 {
                                     writer.AddStringValue("Path", "/etc/letsencrypt/live/spravy.com.ua/fullchain.pem");
                                     writer.AddStringValue("KeyPath", "/etc/letsencrypt/live/spravy.com.ua/privkey.pem");
                                 }
                             );
+                            
+                            Log.Information("Add Certificate");
                         }
                     )
                 );
