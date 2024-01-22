6c73b38806a0f8b5f8443a6458ecf57407cfd4c4
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-01-22 08:28:07 +0200

    Add ToDoItemCanNoneToBooleanReverseConverter and update UI
    A new Converter, ToDoItemCanNoneToBooleanReverseConverter, has been added to handle specific GUI functionality. This converter has been utilized in App.axaml in place of two separate buttons with separate converters. Additionally, a minor adjustment was made to change the ControlTheme key from 'MaterialToggleButton' to 'CheckBoxToggleButton'.

---
+++ Spravy.Ui/Converters/ToDoItemCanNoneToBooleanReverseConverter.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Spravy.ToDo.Domain.Enums;
+
+namespace Spravy.Ui.Converters;
+
+public class ToDoItemCanNoneToBooleanReverseConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not ToDoItemIsCan isCan)
+        {
+            return false;
+        }
+
+        return isCan switch
+        {
+            ToDoItemIsCan.None => false,
+            _ => true
+        };
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
