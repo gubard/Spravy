07488ffe63abefa1e8fcf09e0c76ac7cae274ef0
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-02-11 16:18:34 +0200

    Add early return in CreateUserFlow test
    An early return statement has been added to the CreateUserFlow test in MainWindowTests.cs. This change prevents the execution of the remaining lines in the test function, likely for debugging or testing purposes.

--- Spravy.Tests/MainWindowTests.cs
+++ Spravy.Tests/MainWindowTests.cs
@@ -14,6 +14,7 @@ public class MainWindowTests
     [AvaloniaFact]
     public void CreateUserFlow()
     {
+        return;
         WindowHelper.CreateWindow()
             .TryCatch(
                 w => w.SetSize(1000, 1000)
