a74b3b54f3c5f559ab57630ed5b99ea78876b7ae
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-04-10 10:33:03 +0300

    Replace DatePicker with CalendarDatePicker in UI
    Added a new DateTimeToDateOnlyValueConverter and replaced DateTimeOffsetToDateOnlyValueConverter in the Spravy UI. Additionally, replaced instances of DatePicker control with CalendarDatePicker in various settings view files. Applied necessary style changes to reflect this modification in the calendar view.

---
+++ Spravy.Ui/Converters/DateTimeToDateOnlyValueConverter.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Spravy.Ui.Converters;
+
+public class DateTimeToDateOnlyValueConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not DateOnly dateOnly)
+        {
+            return value;
+        }
+
+        if (DateOnly.MinValue == dateOnly)
+        {
+            return DateTime.MinValue;
+        }
+
+        return new DateTime(dateOnly.Year, dateOnly.Month, dateOnly.Day, 0, 0, 0);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not DateTime dateTimeOffset)
+        {
+            return value;
+        }
+
+        return new DateOnly(dateTimeOffset.Year, dateTimeOffset.Month, dateTimeOffset.Day);
+    }
+}
