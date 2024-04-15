feb635244ec4e707b9d91587f2e8bb9d37b3a3dc
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-04-15 17:05:01 +0300

    Change directory name to lowercase in FileHelper
    The directory name "Spravy" in the GetFrameShortFile method from the FileHelper class has been changed to lowercase "spravy". This change ensures consistency in directory naming conventions across the project.

--- Spravy.Tests/Helpers/FileHelper.cs
+++ Spravy.Tests/Helpers/FileHelper.cs
@@ -6,6 +6,6 @@ public static class FileHelper
 {
     public static FileInfo GetFrameShortFile()
     {
-        return Path.GetTempPath().ToDirectory().Combine("Spravy").ToFile($"{DateTime.Now:yyyy-MM-dd-hh-mm-ss}-{Guid.NewGuid()}.png");
+        return Path.GetTempPath().ToDirectory().Combine("spravy").ToFile($"{DateTime.Now:yyyy-MM-dd-hh-mm-ss}-{Guid.NewGuid()}.png");
     }
 }
