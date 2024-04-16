8129c112a9518bcdcbc65e7157ed2882a188993f
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-04-16 18:13:44 +0300

    Increase job run limit in MainWindowTests
    The number of jobs that can be run in MainWindowTests was doubled from 1ul << 17 to 1ul << 18. This change is implemented to improve execution speed and effectively handle more jobs.

--- Spravy.Tests/MainWindowTests.cs
+++ Spravy.Tests/MainWindowTests.cs
@@ -23,7 +23,7 @@ public class MainWindowTests
                                 () => w.GetCurrentView<LoginView, LoginViewModel>()
                                     .FindControl<Button>(ElementNames.CreateUserButton)
                                     .ThrowIfNull()
-                                    .RunJobsAll(1ul << 17)
+                                    .RunJobsAll(1ul << 18)
                                     .ClickOnButton(w)
                             )
                             .Case(
