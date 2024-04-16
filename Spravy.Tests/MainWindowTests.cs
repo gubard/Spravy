6f901abf6930f824e812f3e62464104454156d88
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-04-16 17:29:46 +0300

    Update RunJobsAll parameter in MainWindowTests
    The parameter for the RunJobsAll function inside the MainWindowTests.cs file has been updated. The previous value (1ul << 15) has been changed to (1ul << 16) to alter the range of jobs run during tests.

--- Spravy.Tests/MainWindowTests.cs
+++ Spravy.Tests/MainWindowTests.cs
@@ -23,7 +23,7 @@ public class MainWindowTests
                                 () => w.GetCurrentView<LoginView, LoginViewModel>()
                                     .FindControl<Button>(ElementNames.CreateUserButton)
                                     .ThrowIfNull()
-                                    .RunJobsAll(1ul << 15)
+                                    .RunJobsAll(1ul << 16)
                                     .ClickOnButton(w)
                             )
                             .Case(
