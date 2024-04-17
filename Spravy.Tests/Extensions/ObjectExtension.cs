bb50b03d14523b855214394914b4ffcea4edbb11
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-04-17 17:35:52 +0300

    Add console logging to RunJobsAll method
    The RunJobsAll method in ObjectExtension.cs now logs console messages to start and end if the count parameter is not equal to 1. Additionally, the method RunJobsAll in MainWindowTests.cs changed the count parameter from '1ul << 6' to '1ul << 7'.

--- Spravy.Tests/Extensions/ObjectExtension.cs
+++ Spravy.Tests/Extensions/ObjectExtension.cs
@@ -6,9 +6,19 @@ public static class ObjectExtension
 {
     public static TObject RunJobsAll<TObject>(this TObject obj, ulong count = 1)
     {
+        if (count != 1)
+        {
+            Console.WriteLine($"RunJobsAll start {count}");
+        }
+        
         for (ulong i = 0; i < count; i++)
         {
             Dispatcher.UIThread.RunJobs();
+        }
+        
+        if (count != 1)
+        {
+            Console.WriteLine($"RunJobsAll end {count}");
         }
 
         return obj;
--- Spravy.Tests/MainWindowTests.cs
+++ Spravy.Tests/MainWindowTests.cs
@@ -24,7 +24,7 @@ public class MainWindowTests
                                     .FindControl<Button>(ElementNames.CreateUserButton)
                                     .ThrowIfNull()
                                     .ClickOnButton(w)
-                                    .RunJobsAll(1ul << 6)
+                                    .RunJobsAll(1ul << 7)
                             )
                             .Case(
                                 () => w.GetCurrentView<CreateUserView, CreateUserViewModel>()
