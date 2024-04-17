2c9dcfabc4f9ea83736f9fbf697b460dcbfdc10c
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-04-17 19:39:08 +0300

    Update task delay method in ObjectExtension
    Changed the method for delaying tasks in the ObjectExtension class. This was done by changing Post() to InvokeAsync() to improve overall performance. Task delay is now set to run when the system is idle.

--- Spravy.Tests/Extensions/ObjectExtension.cs
+++ Spravy.Tests/Extensions/ObjectExtension.cs
@@ -29,7 +29,7 @@ public static class ObjectExtension
     {
         Console.WriteLine("RunJobsAll Start");
         Dispatcher.UIThread.RunJobs();
-        Dispatcher.UIThread.Post(() => Task.Delay(waitTime), DispatcherPriority.MaxValue);
+        Dispatcher.UIThread.InvokeAsync(() => Task.Delay(waitTime), DispatcherPriority.SystemIdle);
         Dispatcher.UIThread.RunJobs();
         Console.WriteLine("RunJobsAll End");
 
