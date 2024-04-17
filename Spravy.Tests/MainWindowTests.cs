e5e931ba57eaad20ee2f0bb4cdbc10ee716b1399
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-04-17 16:36:22 +0300

    Update bit shift in RunJobsAll method call
    The bit shift value for the RunJobsAll method call in the MainWindowTests has been updated from 4 to 5. This change has been implemented to align the method call with changes in the underlying business logic requirements.

--- Spravy.Tests/MainWindowTests.cs
+++ Spravy.Tests/MainWindowTests.cs
@@ -24,7 +24,7 @@ public class MainWindowTests
                                     .FindControl<Button>(ElementNames.CreateUserButton)
                                     .ThrowIfNull()
                                     .ClickOnButton(w)
-                                    .RunJobsAll(1ul << 4)
+                                    .RunJobsAll(1ul << 5)
                             )
                             .Case(
                                 () => w.GetCurrentView<CreateUserView, CreateUserViewModel>()
