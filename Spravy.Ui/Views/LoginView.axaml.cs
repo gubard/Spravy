a713f546d978c6366e3172c55d934017697d842a
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-04-26 17:08:21 +0300

    Update resource bindings in LoginView
    Revised the resource bindings in LoginView.axaml from static to dynamic. This change standardizes the naming convention used for TextBox fields, eliminating the need for constant references in LoginView.axaml.cs. This simplifies the code and improves readability.

--- Spravy.Ui/Views/LoginView.axaml.cs
+++ Spravy.Ui/Views/LoginView.axaml.cs
@@ -5,9 +5,6 @@ namespace Spravy.Ui.Views;
 
 public partial class LoginView : ReactiveUserControl<LoginViewModel>
 {
-    public const string LoginTextBoxName = "LoginTextBox";
-    public const string PasswordTextBoxName = "PasswordTextBox";
-    
     public LoginView()
     {
         InitializeComponent();
