f966e49fce8492bc9a914207f77b4608a1ed6fd0
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-04-26 22:24:51 +0300

    Add console log for button position
    A log statement has been added to the `ClickOnButton` method in the `ButtonExtension` class to print the X and Y coordinates of the button position. This can be useful for debugging or tracking button interaction within the application tests.

--- Spravy.Tests/Extensions/ButtonExtension.cs
+++ Spravy.Tests/Extensions/ButtonExtension.cs
@@ -10,6 +10,7 @@ public static class ButtonExtension
     public static Button ClickOnButton(this Button button, Window window)
     {
         var position = button.TranslatePoint(new Point(0, 0), window).ThrowIfNullStruct().Center(button.Bounds);
+        Console.WriteLine($"X:{position.X}:Y:{position.Y}");
         window.RunJobsAll(1);
         window.SpravyMouseMove(position, RawInputModifiers.None);
         window.RunJobsAll(1);
