83b80e580c1b1373b2c0c181b9ac7c8440749212
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-05-03 16:24:57 +0300

    Update GUID for ToDoItemTypeOutOfRangeError
    The MainId for the ToDoItemTypeOutOfRangeError has been updated. The GUID has been changed from "68A7FCB8-35CB-427F-8A89-A90591519F9B" to "24021BB0-7EA0-4DD2-BDCF-85C42F70C80E". This change ensures the updated unique identifier for the error is reflected throughout the system.

--- Spravy.ToDo.Domain/Errors/ToDoItemTypeOutOfRangeError.cs
+++ Spravy.ToDo.Domain/Errors/ToDoItemTypeOutOfRangeError.cs
@@ -5,7 +5,7 @@ namespace Spravy.ToDo.Domain.Errors;
 
 public class ToDoItemTypeOutOfRangeError : ValueOutOfRangeError<ToDoItemType>
 {
-    public static readonly Guid MainId = new("68A7FCB8-35CB-427F-8A89-A90591519F9B");
+    public static readonly Guid MainId = new("24021BB0-7EA0-4DD2-BDCF-85C42F70C80E");
     
     protected ToDoItemTypeOutOfRangeError() : base(ToDoItemType.Value, MainId)
     {
