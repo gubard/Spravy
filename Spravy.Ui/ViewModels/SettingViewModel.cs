8da84d1bdb4c0e5e250efb7ae6a17564a92df7a3
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-05-04 14:35:27 +0300

    Add save setting notification
    The code has been updated to show a notification after settings are saved. Previously, the notification feature wasn't implemented in the settings page. The new feature enhances user experience by providing immediate feedback after an action is performed.

--- Spravy.Ui/ViewModels/SettingViewModel.cs
+++ Spravy.Ui/ViewModels/SettingViewModel.cs
@@ -4,12 +4,12 @@ public class SettingViewModel : NavigatableViewModelBase
 {
     private readonly PageHeaderViewModel pageHeaderViewModel;
     private readonly SukiTheme theme = SukiTheme.GetInstance();
-
+    
     public SettingViewModel() : base(true)
     {
         InitializedCommand = CreateInitializedCommand(TaskWork.Create(InitializedAsync).RunAsync);
         AvailableColors = new(theme.ColorThemes.Select(x => new Selected<SukiColorTheme>(x)));
-
+        
         foreach (var availableColor in AvailableColors)
         {
             if (availableColor.Value == theme.ActiveColorTheme)
@@ -17,48 +17,51 @@ public class SettingViewModel : NavigatableViewModelBase
                 availableColor.IsSelect = true;
             }
         }
-
+        
         IsLightTheme = theme.ActiveBaseTheme == ThemeVariant.Light;
     }
-
+    
     public override string ViewId
     {
         get => TypeCache<SettingViewModel>.Type.Name;
     }
-
+    
     public ICommand InitializedCommand { get; }
-
+    
+    [Inject]
+    public required ISpravyNotificationManager SpravyNotificationManager { get; init; }
+    
     public string Version
     {
         get
         {
             var versionString = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? string.Empty;
-
+            
             if (versionString.IsNullOrWhiteSpace())
             {
                 return "1.0.0.0(0)";
             }
-
+            
             if (!SpravyVersion.TryParse(versionString, out var version))
             {
                 return "1.0.0.0(0)";
             }
-
+            
             return $"{version}({version.Code})";
         }
     }
-
+    
     public AvaloniaList<Selected<SukiColorTheme>> AvailableColors { get; }
-
+    
     [Inject]
     public required AccountNotify AccountNotify { get; init; }
-
+    
     [Inject]
     public required IAuthenticationService AuthenticationService { get; init; }
-
+    
     [Inject]
     public required IObjectStorage ObjectStorage { get; init; }
-
+    
     [Inject]
     public required PageHeaderViewModel PageHeaderViewModel
     {
@@ -71,54 +74,54 @@ public class SettingViewModel : NavigatableViewModelBase
             Disposables.Add(pageHeaderViewModel);
         }
     }
-
+    
     [Reactive]
     public ICommand ChangePasswordCommand { get; protected set; }
-
+    
     [Reactive]
     public ICommand DeleteAccountCommand { get; protected set; }
-
+    
     [Reactive]
     public ICommand SwitchToColorThemeCommand { get; protected set; }
-
+    
     [Reactive]
     public ICommand SaveSettingsCommand { get; protected set; }
-
+    
     [Reactive]
     public bool IsBusy { get; set; }
-
+    
     [Reactive]
     public bool IsLightTheme { get; set; }
-
+    
     private ConfiguredValueTaskAwaitable<Result> InitializedAsync(CancellationToken cancellationToken)
     {
         ChangePasswordCommand = CreateCommandFromTask(TaskWork.Create(ChangePasswordAsync).RunAsync);
         SaveSettingsCommand = CreateCommandFromTask(TaskWork.Create(SaveSettingsAsync).RunAsync);
         DeleteAccountCommand = CreateCommandFromTask(TaskWork.Create(DeleteAccountAsync).RunAsync);
-
+        
         SwitchToColorThemeCommand =
             CreateCommandFromTask<Selected<SukiColorTheme>>(TaskWork
                .Create<Selected<SukiColorTheme>>(SwitchToColorTheme)
                .RunAsync);
-
+        
         Disposables.Add(this.WhenAnyValue(x => x.IsLightTheme)
            .Subscribe(x => theme.ChangeBaseTheme(x ? ThemeVariant.Light : ThemeVariant.Dark)));
-
+        
         return Result.AwaitableFalse;
     }
-
+    
     private ConfiguredValueTaskAwaitable<Result> SaveSettingsAsync(CancellationToken cancellationToken)
     {
-        return this.InvokeUIBackgroundAsync(() => IsBusy = true)
-           .IfSuccessTryFinallyAsync(() => ObjectStorage.SaveObjectAsync(TypeCache<SettingModel>.Type.Name,
-                    new SettingModel
-                    {
-                        BaseTheme = IsLightTheme ? "Light" : "Dark",
-                        ColorTheme = AvailableColors.Single(x => x.IsSelect).Value.DisplayName,
-                    }), () => this.InvokeUIBackgroundAsync(() => IsBusy = false).ToValueTask().ConfigureAwait(false),
-                cancellationToken);
+        return ObjectStorage.SaveObjectAsync(TypeCache<SettingModel>.Type.Name, new SettingModel
+            {
+                BaseTheme = IsLightTheme ? "Light" : "Dark",
+                ColorTheme = AvailableColors.Single(x => x.IsSelect).Value.DisplayName,
+            })
+           .IfSuccessAsync(
+                () => SpravyNotificationManager.ShowAsync(new TextLocalization("SettingView.SaveSetting"),
+                    cancellationToken), cancellationToken);
     }
-
+    
     private ConfiguredValueTaskAwaitable<Result> DeleteAccountAsync(CancellationToken cancellationToken)
     {
         return Navigator.NavigateToAsync<DeleteAccountViewModel>(vm =>
@@ -127,7 +130,7 @@ public class SettingViewModel : NavigatableViewModelBase
             vm.IdentifierType = UserIdentifierType.Login;
         }, cancellationToken);
     }
-
+    
     private ConfiguredValueTaskAwaitable<Result> ChangePasswordAsync(CancellationToken cancellationToken)
     {
         return Navigator.NavigateToAsync<ForgotPasswordViewModel>(vm =>
@@ -136,17 +139,17 @@ public class SettingViewModel : NavigatableViewModelBase
             vm.IdentifierType = UserIdentifierType.Login;
         }, cancellationToken);
     }
-
+    
     public override Result Stop()
     {
         return Result.Success;
     }
-
+    
     public override ConfiguredValueTaskAwaitable<Result> SaveStateAsync(CancellationToken cancellationToken)
     {
         return Result.AwaitableFalse;
     }
-
+    
     public override ConfiguredValueTaskAwaitable<Result> SetStateAsync(
         object setting,
         CancellationToken cancellationToken
@@ -154,7 +157,7 @@ public class SettingViewModel : NavigatableViewModelBase
     {
         return Result.AwaitableFalse;
     }
-
+    
     public ConfiguredValueTaskAwaitable<Result> SwitchToColorTheme(
         Selected<SukiColorTheme> colorTheme,
         CancellationToken cancellationToken
