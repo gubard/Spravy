e4644b6e75c19a82c5abed0175255ba54915af35
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-05-11 17:33:04 +0300

    Add constructor and command to ActiveToDoItemNotify
    Added a constructor to the ActiveToDoItemNotify class which accepts the id, navigator, and error handler parameters. Also, introduced a new command, NavigateToCurrentItem, which uses the provided navigator to navigate to the ToDoItemViewModel page while handling potential errors. Extended the `Name` property to initialize with empty string by default.

--- Spravy.Ui/Features/ToDo/Models/ActiveToDoItemNotify.cs
+++ Spravy.Ui/Features/ToDo/Models/ActiveToDoItemNotify.cs
@@ -2,9 +2,21 @@ namespace Spravy.Ui.Features.ToDo.Models;
 
 public class ActiveToDoItemNotify : NotifyBase
 {
+    public ActiveToDoItemNotify(Guid id, INavigator navigator, IErrorHandler errorHandler)
+    {
+        Id = id;
+        
+        NavigateToCurrentItem =
+            SpravyCommand.Create(
+                cancellationToken => navigator.NavigateToAsync<ToDoItemViewModel>(vm => vm.Id = Id, cancellationToken),
+                errorHandler);
+    }
+    
+    public SpravyCommand NavigateToCurrentItem { get; }
+    
     [Reactive]
     public Guid Id { get; set; }
     
     [Reactive]
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 }
