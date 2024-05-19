ba724719ce8615d70e53f658305c39db0d3bd631
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-05-19 17:16:46 +0300

    Added IsMulti functionality to MultiToDoItemsView
    This update enriches the MultiToDoItemsView by adding a new property, IsMulti, and correlating PseudoClasses functionality. Consequently, the UI can now react and adjust to changes in the multi-state of the ToDo items. A new StyledProperty is defined to facilitate two-way binding of this property, thus ensuring synchronization between the UI and the underlying ViewModel. This change also includes a modification in the ax

--- Spravy.Ui/Features/ToDo/Views/MultiToDoItemsView.axaml.cs
+++ Spravy.Ui/Features/ToDo/Views/MultiToDoItemsView.axaml.cs
@@ -1,11 +1,40 @@
+using Avalonia.Data;
+
 namespace Spravy.Ui.Features.ToDo.Views;
 
+[PseudoClasses(PseudoClassesIsMulti)]
 public partial class MultiToDoItemsView : ReactiveUserControl<MultiToDoItemsViewModel>
 {
     public const string ContentContentControlName = "content-content-control";
-
+    public const string PseudoClassesIsMulti = ":is-multi";
+    
+    public static readonly StyledProperty<bool> IsMultiProperty =
+        AvaloniaProperty.Register<MultiToDoItemsView, bool>(nameof(IsMulti), defaultBindingMode: BindingMode.TwoWay);
+    
     public MultiToDoItemsView()
     {
         InitializeComponent();
+    }
+    
+    public bool IsMulti
+    {
+        get => GetValue(IsMultiProperty);
+        set
+        {
+            SetValue(IsMultiProperty, value);
+            UpdatePseudoClasses();
+        }
+    }
+    
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+        
+        UpdatePseudoClasses();
+    }
+    
+    private void UpdatePseudoClasses()
+    {
+        PseudoClasses.Set(PseudoClassesIsMulti, IsMulti);
     }
 }
