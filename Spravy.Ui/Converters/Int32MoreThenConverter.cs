39f276b39f6e93421b2fa06f157ed57af2208d90
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-06-19 09:27:43 +0300

    Add Int32MoreThenConverter and update views
    New file 'Int32MoreThenConverter.cs' has been introduced to facilitate data conversions from Int32, with 'IsEquals', 'IsReverse' properties, and 'Convert' and 'ConvertBack' methods. This converter is then being used in 'ToDoItemsView.axaml' to conditionally display key user interface elements based on list count, which improves usability for long lists. Ensure appropriate styles are included in 'Theme.axaml'.

---
+++ Spravy.Ui/Converters/Int32MoreThenConverter.cs
@@ -0,0 +1,45 @@
+namespace Spravy.Ui.Converters;
+
+public class Int32MoreThenConverter : IValueConverter
+{
+    public bool IsReverse { get; set; }
+    public bool IsEquals { get; set; }
+
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not int v)
+        {
+            return false;
+        }
+        
+        if (parameter is not int p)
+        {
+            if (parameter is not string str)
+            {
+                return false;
+            }
+
+            if (!int.TryParse(str, out p))
+            {
+                return false; 
+            }
+        }
+
+        if (IsEquals && v == p)
+        {
+            return true;
+        }
+
+        if (IsReverse)
+        {
+            return v < p;
+        }
+        
+        return v > p;
+    }
+    
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
--- Spravy.Ui/Features/ToDo/Views/ToDoItemsView.axaml.cs
+++ Spravy.Ui/Features/ToDo/Views/ToDoItemsView.axaml.cs
@@ -3,6 +3,7 @@ namespace Spravy.Ui.Features.ToDo.Views;
 public partial class ToDoItemsView : ReactiveUserControl<ToDoItemsViewModel>
 {
     public const string ItemsItemsControlName = "items-items-control";
+    public const string ItemsItemsControlVirtualizingStackPanelName = "items-items-control-virtualizing-stack-panel";
 
     public ToDoItemsView()
     {
