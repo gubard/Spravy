f1de4ce8a424691661257e5d03015cdc235f3d91
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-06-24 12:06:11 +0300

    Update project settings and dependencies
    This commit consolidates project settings into the Directory.Build.props file and updates the FluentFTP version for the build. It removes explicit configuration directives from each individual .csproj file, providing a centralized way to manage these settings. Also, the FluentFTP package in _build.csproj got updated to version "50.1.0".

--- Spravy.Ui.Android/Services/AndroidOpenerLink.cs
+++ Spravy.Ui.Android/Services/AndroidOpenerLink.cs
@@ -1,19 +1,16 @@
-using System;
 using System.Runtime.CompilerServices;
-using System.Threading;
 using Android.Content;
-using Spravy.Domain.Models;
 using Spravy.Ui.Interfaces;
 
 namespace Spravy.Ui.Android.Services;
 
 public class AndroidOpenerLink : IOpenerLink
 {
-    public ConfiguredValueTaskAwaitable<Result> OpenLinkAsync(Uri link, CancellationToken cancellationToken)
+    public ConfiguredValueTaskAwaitable<Spravy.Domain.Models.Result> OpenLinkAsync(Uri link, CancellationToken cancellationToken)
     {
         var intent = new Intent(Intent.ActionView, global::Android.Net.Uri.Parse(link.AbsoluteUri));
         MainActivity.Instance.StartActivity(intent);
 
-        return Result.AwaitableSuccess;
+        return Domain.Models.Result.AwaitableSuccess;
     }
 }
