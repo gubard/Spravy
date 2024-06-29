234ff17f82310672b16ad1c4f2f78041f46b7f74
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-06-29 13:36:27 +0300

    Add adaptive behavior to MainView and refactor MainWindow constructor
    This commit includes a significant update to the MainView, introducing adaptive behavior to enhance the user interface. It includes conditional visibility and parameters based on window size for several controls. Also, the MainWindow constructor was updated to receive MainViewModel instead of ISingleViewTopLevelControl, following a change in the application's structure. These alterations aim for better adaptability and increased maintainability of the code.

--- Spravy.Ui/Views/MainWindow.axaml.cs
+++ Spravy.Ui/Views/MainWindow.axaml.cs
@@ -2,9 +2,9 @@ namespace Spravy.Ui.Views;
 
 public partial class MainWindow : SukiWindow, IDesktopTopLevelControl
 {
-    public MainWindow(ISingleViewTopLevelControl singleView)
+    public MainWindow(MainViewModel viewModel)
     {
         InitializeComponent();
-        Content = singleView;
+        Content = viewModel;
     }
 }
