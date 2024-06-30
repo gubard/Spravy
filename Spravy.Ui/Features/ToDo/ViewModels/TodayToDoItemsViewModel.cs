2520081e1fd00ad0163c5134af2958d39d6ab932
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-06-30 06:26:41 +0300

    Simplify to-do items group styles in MainView
    The commit removes a lot of detailed style selectors related to to-do items grouping in MainView.axaml and simplifies the naming to reflect general usage. It reduces code duplication and complexity in the style selectors by removing the adaptive behavior and replacing it with a simplified className. This streamlines the UI code and improves readability.

--- Spravy.Ui/Features/ToDo/ViewModels/TodayToDoItemsViewModel.cs
+++ Spravy.Ui/Features/ToDo/ViewModels/TodayToDoItemsViewModel.cs
@@ -22,6 +22,7 @@ public class TodayToDoItemsViewModel
         ToDoSubItemsViewModel = toDoSubItemsViewModel;
         this.toDoService = toDoService;
         this.toDoCache = toDoCache;
+
         InitializedCommand = SpravyCommand.Create(
             InitializedAsync,
             errorHandler,
