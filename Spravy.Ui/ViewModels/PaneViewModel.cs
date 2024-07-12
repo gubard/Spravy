8f9ecc9a2f9226008d5b982bc9c98df1f41e4537
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-07-12 15:41:55 +0300

    Add conditional visibility to PasswordGenerator button
    The PasswordGenerator button in PaneView is now only visible for certain users. This visibility is handled through a binding to the new IsShowPasswordGenerator property in the PaneViewModel. This property returns true only if the Account's Login is either "vafnir" or "admin".

--- Spravy.Ui/ViewModels/PaneViewModel.cs
+++ Spravy.Ui/ViewModels/PaneViewModel.cs
@@ -5,7 +5,16 @@ public class PaneViewModel : ViewModelBase
     public PaneViewModel(AccountNotify account)
     {
         Account = account;
+
+        Account
+            .WhenAnyValue(x => x.Login)
+            .Subscribe(_ => this.RaisePropertyChanged(nameof(IsShowPasswordGenerator)));
     }
 
     public AccountNotify Account { get; }
+
+    public bool IsShowPasswordGenerator
+    {
+        get => Account.Login is "vafnir" or "admin";
+    }
 }
