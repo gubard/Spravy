dac240628f9356b90e878ccd798cc67dbe03d7ed
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-07-24 09:53:45 +0300

    Update PathControl to support separators
    The PathControl has been updated to support customizable separators between segments, replacing the previous "Steps" concept with "Segments". Also, this refactoring included a change in all axaml files where the PathControl is used, replacing the "Steps" with "Segments". Now, the control is more flexible and capable of displaying a wider variety of paths.

--- Spravy.Ui/Controls/PathControl.cs
+++ Spravy.Ui/Controls/PathControl.cs
@@ -1,13 +1,23 @@
 using Avalonia.Data;
+using Material.Icons.Avalonia;
 
 namespace Spravy.Ui.Controls;
 
 public class PathControl : TemplatedControl
 {
-    public static readonly StyledProperty<IEnumerable> StepsProperty = AvaloniaProperty.Register<
+    private static readonly FuncTemplate<Control?> defaultSeparator =
+        new(() => new MaterialIcon { Kind = MaterialIconKind.ChevronRight, });
+
+    public static readonly StyledProperty<ITemplate<Control?>> SeparatorProperty =
+        AvaloniaProperty.Register<PathControl, ITemplate<Control?>>(
+            nameof(SeparatorPanel),
+            defaultSeparator
+        );
+
+    public static readonly StyledProperty<IEnumerable> SegmentsProperty = AvaloniaProperty.Register<
         PathControl,
         IEnumerable
-    >(nameof(Steps), defaultBindingMode: BindingMode.TwoWay);
+    >(nameof(Segments), defaultBindingMode: BindingMode.TwoWay);
 
     public static readonly StyledProperty<IEnumerable> ItemsProperty = AvaloniaProperty.Register<
         PathControl,
@@ -16,8 +26,22 @@ public class PathControl : TemplatedControl
 
     static PathControl()
     {
-        StepsProperty.Changed.AddClassHandler<PathControl>(
-            (control, _) => control.Items = control.Steps.OfType<object>().ToArray()
+        SegmentsProperty.Changed.AddClassHandler<PathControl>(
+            (control, _) =>
+            {
+                var segments = control.Segments.OfType<object>().ToArray().AsSpan();
+                var body = segments[..^1];
+                var items = new object[segments.Length * 2 - 1].AsSpan();
+
+                for (var i = 0; i < body.Length; i++)
+                {
+                    items[i * 2] = body[i];
+                    items[i * 2 + 1] = control.SeparatorPanel.Build() ?? new object();
+                }
+
+                items[^1] = segments[^1];
+                control.Items = items.ToArray();
+            }
         );
     }
 
@@ -26,10 +50,16 @@ public class PathControl : TemplatedControl
         Items = Array.Empty<object>();
     }
 
-    public IEnumerable Steps
+    public ITemplate<Control?> SeparatorPanel
     {
-        get => GetValue(StepsProperty);
-        set => SetValue(StepsProperty, value);
+        get => GetValue(SeparatorProperty);
+        set => SetValue(SeparatorProperty, value);
+    }
+
+    public IEnumerable Segments
+    {
+        get => GetValue(SegmentsProperty);
+        set => SetValue(SegmentsProperty, value);
     }
 
     public IEnumerable Items
