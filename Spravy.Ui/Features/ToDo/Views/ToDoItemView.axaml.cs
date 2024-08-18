159d89be12e3487795f02c0d9c0960b6fe71bc3d
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-08-18 07:40:21 +0300

    Remove redundant constants and simplify MarkdownScrollViewer.
    Eliminated unused constants from the ToDoItemView class to clean up the codebase. Also, removed unnecessary classes from MarkdownScrollViewer for a more straightforward implementation.

--- Spravy.Ui/Features/ToDo/Views/ToDoItemView.axaml.cs
+++ Spravy.Ui/Features/ToDo/Views/ToDoItemView.axaml.cs
@@ -2,11 +2,6 @@ namespace Spravy.Ui.Features.ToDo.Views;
 
 public partial class ToDoItemView : MainUserControl<ToDoItemViewModel>
 {
-    public const string PlainTextTextBlockName = "plain-text-text-block";
-
-    public const string MarkdownTextMarkdownScrollViewerName =
-        "markdown-text-markdown-scroll-viewer";
-
     public ToDoItemView()
     {
         InitializeComponent();
