20292008c592797d3554a55f47ae7554ea79064c
gubard <maksimovsergey08@gmail.com>
2024-08-22 22:03:24 +0300

    Add TimeSpanAddItemControl stylesheet and improve ListControl
    Created a new stylesheet for TimeSpanAddItemControl in XAML to define a template with a TimePicker. Modified ListControl by adding Complete and Cancel buttons and updated item handling logic to ensure proper interaction with the new controls.

--- Spravy.Ui/Controls/AddItemControl.cs
+++ Spravy.Ui/Controls/AddItemControl.cs
@@ -1,6 +1,6 @@
 namespace Spravy.Ui.Controls;
 
-public abstract class AddItemControl : Control, IAddItem
+public abstract class AddItemControl : TemplatedControl, IAddItem
 {
     public abstract object Value { get; }
 }
