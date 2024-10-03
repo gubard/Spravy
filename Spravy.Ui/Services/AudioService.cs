8087c385937cba3c46e8f51d20cc32ee89348372
gubard <maksimovsergey08@gmail.com>
2024-10-03 11:31:46 +0300

    Remove NAudio dependency and related audio functionality
    Deleted `WaveStreamExtensions.cs` and removed NAudio usage in `AudioService.cs`. Updated project file to exclude NAudio package reference. The audio play logic is simplified, removing the need for NAudio.

--- Spravy.Ui/Services/AudioService.cs
+++ Spravy.Ui/Services/AudioService.cs
@@ -1,29 +1,9 @@
-using Avalonia.Platform;
-using NAudio.Wave;
-
 namespace Spravy.Ui.Services;
 
 public class AudioService : IAudioService
 {
-    private const string AudioFileCompletePath = "avares://Spravy.Ui/Assets/Sounds/Complete.mp3";
-    private static readonly Uri audioFileCompleteUri = new(AudioFileCompletePath);
-
     public Cvtar PlayCompleteAsync(CancellationToken ct)
     {
-        return PlayCompleteCore(ct).ConfigureAwait(false);
-    }
-
-    private async ValueTask<Result> PlayCompleteCore(CancellationToken ct)
-    {
-        await using var stream = AssetLoader.Open(audioFileCompleteUri);
-        await using var reader = new Mp3FileReader(stream);
-        reader.Skip(0.1);
-        using var outputDevice = new WaveOutEvent();
-        outputDevice.Init(reader);
-        outputDevice.Play();
-        await Task.Delay(TimeSpan.FromMilliseconds(500), ct);
-        outputDevice.Stop();
-
-        return Result.Success;
+        return Result.AwaitableSuccess;
     }
 }
