7f254970e3fb945f6c5709c6c79499afa9ace254
Serhii Maksymov <maksimovsergey08@gmail.com>
2024-10-17 18:00:35 +0300

    Add ColorToTextForegroundConverter for dynamic text color
    Introduced a new ColorToTextForegroundConverter to handle dynamic text color changes based on the bound color value. Applied the converter across relevant views and updated resources to include this new converter.

---
+++ Spravy.Ui/Converters/ColorToTextForegroundConverter.cs
@@ -0,0 +1,58 @@
+namespace Spravy.Ui.Converters;
+
+public class ColorToTextForegroundConverter : IValueConverter
+{
+    private static Application application;
+
+    static ColorToTextForegroundConverter()
+    {
+        application = Application.Current.ThrowIfNull();
+    }
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not Color color)
+        {
+            if (
+                application.TryFindResource(
+                    "TextControlForeground",
+                    application.RequestedThemeVariant,
+                    out var resource
+                )
+            )
+            {
+                return resource;
+            }
+
+            return Brushes.Red;
+        }
+
+        if (color.A == 0)
+        {
+            if (
+                application.TryFindResource(
+                    "TextControlForeground",
+                    application.RequestedThemeVariant,
+                    out var resource
+                )
+            )
+            {
+                return resource;
+            }
+
+            return Brushes.Red;
+        }
+
+        return new SolidColorBrush(color);
+    }
+
+    public object? ConvertBack(
+        object? value,
+        Type targetType,
+        object? parameter,
+        CultureInfo culture
+    )
+    {
+        throw new NotSupportedException();
+    }
+}
