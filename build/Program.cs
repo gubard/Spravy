7b5cc70feb6377762357dd0975dd46363615d025
gubard <maksimovsergey08@gmail.com>
2025-01-06 13:13:31 +0200

    Refactor build argument handling in Program.cs
    Replaced hardcoded indexing with dynamic detection of the "--build" argument. This improves flexibility and ensures proper handling of varying argument orders.

--- build/Program.cs
+++ build/Program.cs
@@ -6,7 +6,21 @@ public static class Program
 {
     static int Main(string[] args)
     {
-        return args[1] switch
+        var buildIndex = 0;
+
+        for (var index = 0; index < args.Length; index++)
+        {
+            var arg = args[index];
+
+            if (arg == "--build")
+            {
+                buildIndex = index;
+
+                break;
+            }
+        }
+
+        return args[buildIndex + 1] switch
         {
             "publish" => Build.Execute(),
             "test" => TestBuild.Execute(),
