a979d2343181e05e6fd3949f0ce76d58b21d8087
gubard <maksimovsergey08@gmail.com>
2025-01-08 07:23:20 +0200

    Add LinesSubstringConverter to limit displayed text lines
    Introduced a new `LinesSubstringConverter` class to control the number of lines displayed in text fields. Updated `MultiToDoItemsView.axaml` to use the converter for the `Description` field and registered the converter as a resource in `Resources.axaml` with a default `MaxLines` of 5. This ensures improved readability for long text content.

---
+++ Spravy.Ui/Converters/LinesSubstringConverter.cs
@@ -0,0 +1,28 @@
+﻿namespace Spravy.Ui.Converters;
+
+public class LinesSubstringConverter : IValueConverter
+{
+    public ushort MaxLines { get; set; }
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not string str)
+        {
+            return value;
+        }
+
+        return string.Join(
+            Environment.NewLine,
+            str.Split(
+                    '\r',
+                    '\n'
+                )
+               .Take(MaxLines)
+        );
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
