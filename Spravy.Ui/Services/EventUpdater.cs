993d0e3ca62145e53e7e56980e58fe24aebdf87a
gubard <maksimovsergey08@gmail.com>
2025-01-08 13:51:23 +0200

    Refactor EventUpdater to simplify dependencies.
    Removed unused services and streamlined the update process by focusing solely on refreshing the current view. This reduces complexity, eliminates redundant code, and enhances maintainability.

--- Spravy.Ui/Services/EventUpdater.cs
+++ Spravy.Ui/Services/EventUpdater.cs
@@ -2,26 +2,17 @@ namespace Spravy.Ui.Services;
 
 public class EventUpdater : IEventUpdater
 {
-    private readonly IAudioService audioService;
-    private readonly IScheduleService scheduleService;
     private readonly ISpravyNotificationManager spravyNotificationManager;
-    private readonly IToDoService toDoService;
     private readonly IUiApplicationService uiApplicationService;
     private CancellationTokenSource cancellationTokenSource = new();
 
     public EventUpdater(
-        IScheduleService scheduleService,
         ISpravyNotificationManager spravyNotificationManager,
-        IToDoService toDoService,
-        IUiApplicationService uiApplicationService,
-        IAudioService audioService
+        IUiApplicationService uiApplicationService
     )
     {
-        this.scheduleService = scheduleService;
         this.spravyNotificationManager = spravyNotificationManager;
-        this.toDoService = toDoService;
         this.uiApplicationService = uiApplicationService;
-        this.audioService = audioService;
     }
 
     public void Start()
@@ -44,36 +35,7 @@ public class EventUpdater : IEventUpdater
         {
             try
             {
-                await scheduleService.UpdateEventsAsync(ct)
-                   .IfSuccessAsync(
-                        isUpdated =>
-                        {
-                            if (!isUpdated)
-                            {
-                                return Result.AwaitableSuccess;
-                            }
-
-                            return spravyNotificationManager.Show(new TextLocalization("Lang.UpdateSchedule"))
-                               .IfSuccessAsync(() => audioService.PlayNotificationAsync(ct), ct)
-                               .IfSuccessAsync(() => toDoService.UpdateEventsAsync(ct), ct)
-                               .IfSuccessAsync(
-                                    x =>
-                                    {
-                                        if (!x)
-                                        {
-                                            return uiApplicationService.RefreshCurrentViewAsync(ct);
-                                        }
-
-                                        return spravyNotificationManager
-                                           .Show(new TextLocalization("lang.UpdateToDoEvents"))
-                                           .IfSuccessAsync(() => audioService.PlayNotificationAsync(ct), ct)
-                                           .IfSuccessAsync(() => uiApplicationService.RefreshCurrentViewAsync(ct), ct);
-                                    },
-                                    ct
-                                );
-                        },
-                        ct
-                    )
+                await uiApplicationService.RefreshCurrentViewAsync(ct)
                    .IfErrorsAsync(
                         errors => spravyNotificationManager.Show(
                             errors.Select(x => $"{x.Id}{Environment.NewLine}{x.Message}")
