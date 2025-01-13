78599d0e0d3ccc05a483e4fded7755eec7177f24
gubard <maksimovsergey08@gmail.com>
2025-01-13 11:38:59 +0200

    Refactor EventUpdater to remove unused logic and dependencies
    Removed unnecessary dependencies, including logging and application service, and simplified `StartCore` to return a completed `ValueTask`. This reduces complexity and streamlines the class for better maintainability.

--- Spravy.Ui/Services/EventUpdater.cs
+++ Spravy.Ui/Services/EventUpdater.cs
@@ -1,18 +1,8 @@
-using Serilog;
-
 namespace Spravy.Ui.Services;
 
 public class EventUpdater : IEventUpdater
 {
-    private readonly IUiApplicationService uiApplicationService;
     private CancellationTokenSource cancellationTokenSource = new();
-
-    public EventUpdater(
-        IUiApplicationService uiApplicationService
-    )
-    {
-        this.uiApplicationService = uiApplicationService;
-    }
 
     public void Start()
     {
@@ -26,31 +16,8 @@ public class EventUpdater : IEventUpdater
         cancellationTokenSource = new();
     }
 
-    private async ValueTask StartCore()
+    private ValueTask StartCore()
     {
-        var ct = cancellationTokenSource.Token;
-
-        while (!ct.IsCancellationRequested)
-        {
-            try
-            {
-                await uiApplicationService.RefreshCurrentViewAsync(ct)
-                   .IfErrorsAsync(
-                        errors =>
-                        {
-                            Log.Logger.Error("Event errors: {Errors}", errors);
-
-                            return Result.Success;
-                        },
-                        ct
-                    );
-            }
-            catch (Exception e)
-            {
-                Log.Logger.Error(e, "Event exception");
-            }
-
-            await Task.Delay(TimeSpan.FromMinutes(1), ct);
-        }
+        return ValueTask.CompletedTask;
     }
 }
